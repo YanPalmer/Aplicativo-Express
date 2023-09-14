9ad39804d6a7a9fb0ab0ad23d0be4f5caf592569
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-09-14 18:55:32 -0300

    Código do express para aprendizado

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 console.log("Server is starting");
 
 const express = require("express");
-
 const app = express();
-
+// const port = 3000; Para configurar uma porta como uma variável
 const server = app.listen(3000, listening());
 
 function listening() {
@@ -11,3 +10,15 @@ function listening() {
 }
 
 app.use(express.static("website"));
+
+/*
+// Responde no html com "Hello World" a solicitações para a URL raiz (/)
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Escuta na porta especificada e printa na tela o console.log();
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+*/
