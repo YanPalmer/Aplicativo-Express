916625295fc9dd64a92f194e0fed9ffac3e0b4c0
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-09-14 14:54:45 -0300

    Criei um servidor que escuta a porta 3000

---
+++ server.js
@@ -0,0 +1,13 @@
+console.log("Server is starting");
+
+const express = require("express");
+
+const app = express();
+
+const server = app.listen(3000, listening());
+
+function listening() {
+    console.log("Listening. . .");
+}
+
+app.use(express.static("website"));
