f206ba0fa09f32c2591660332fc45f3cd822de04
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-10-12 14:58:50 -0300

    Brinquei com as funcionalidades do Geolocation

--- public/script.js
+++ public/script.js
@@ -5,11 +5,46 @@ if ("geolocation" in navigator) {
         const lon = position.coords.longitude;
         document.getElementById('latitude').textContent = lat;
         document.getElementById('longitude').textContent = lon;
-        var map = L.map('map').setView([lat, lon], 13);
+        var map = L.map('map').setView([lat, lon], 15);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
+
+        var marker = L.marker([lat, lon]).addTo(map).bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
+
+        var circle = L.circle([lat, lon], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 100
+        }).addTo(map).bindPopup("I am a circle.");
+
+        var polygon = L.polygon([
+            [-8.276996, -35.967307],
+            [-8.279247, -35.968058],
+            [-8.278597, -35.969791],
+            [-8.277355, -35.969753],
+            [-8.276554, -35.967854]
+        ]).addTo(map)
+        // .bindPopup("I am a polygon.")
+        .bindPopup("It's where i work")
+        ;
+
+        const popup = L.popup()
+            .setLatLng([lat, lon])
+            .setContent("I am a standalone popup.")
+            .openOn(map);
+
+        function onMapClick(e) {
+            // console.log(e.latlng);
+            popup
+                .setLatLng(e.latlng)
+                .setContent(`You clicked the map at ${e.latlng.toString()}`)
+                .openOn(map);
+        }
+
+        map.on('click', onMapClick);
     });
 } else {
     console.log("geolocation IS NOT available");
