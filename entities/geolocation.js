7b301f06e3ec24e8e9a5dece231fbe3181050376
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-10-19 22:02:09 -0300

    typeORM com JavaScript
    Aprendendo como acessar o typeORM pelo JavaScript

--- entities/geolocation.js
+++ entities/geolocation.js
@@ -1,13 +1,32 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { EntitySchema } from "typeorm";
 
-Entity()
 export class Geolocation {
-    @PrimaryGeneratedColumn()
-    id;
-
-    @Column()
-    Latitude;
-    
-    @Column()
-    Longitude;
+    constructor(id, Latitude, Longitude) {
+        this.id = id;
+        this.Latitude = Latitude
+        this.Longitude = Longitude
+    }
 }
+
+export default new EntitySchema({
+    // Nome da tabela
+    name: 'Geolocation Table',
+    // Seleciona a classe
+    target: Geolocation,
+    columns: {
+        id: {
+            // Chave primária
+            primary: true,
+            type: 'int',
+            // Incrementa automaticamente
+            generated: true,
+        },
+        Latitude: {
+            type: 'number',
+        },
+        Longitude: {
+            type: 'number',
+        }
+    }
+})
